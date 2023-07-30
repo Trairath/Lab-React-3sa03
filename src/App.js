14e5533be27be84141e0afd0f6e3f2c66998540f
Trairath <6210110712@psu.ac.th>
2023-07-31 00:29:10 +0700

    add random word

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import WordCard from './WordCard';
+import _ from 'lodash'
 
 function App() {
+  const word = ["hello","about","actor","crime","clear","begin","drunk","enjoy",
+                "every","faith","fault","group","habit","heavy","judge","knife"];
+
+  const randomElement = _.sample(word);
   return (
     <div>
-      <WordCard value="hello"/>
+      <WordCard value={randomElement}/>
     </div>
   );
 }
