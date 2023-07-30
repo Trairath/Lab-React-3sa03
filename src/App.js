fa2cca896ce3b7fd22a08dfd17fe83f79a4032ec
Trairath <6210110712@psu.ac.th>
2023-07-30 19:02:52 +0700

    More

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 
-const word = "hello";
 function App() {
   return (
-    <div >
-      {
-      Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
-      }
+    <div>
+      <WordCard value="hello"/>
     </div>
   );
 }
